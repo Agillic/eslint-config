af44478ad9620faa71e030d2c86616d584c7f70f
Krzysztof Czuryło <krzysztof.czurylo@agillic.com>
2019-03-13 16:04:04 +1100

    Initial config

---
+++ index.js
@@ -0,0 +1,88 @@
+module.exports = {
+  "extends": ["airbnb", "plugin:sonarjs/recommended"],
+  "parser": "babel-eslint",
+  "plugins": ["fp", "unicorn", "sonarjs", "array-func", "immutable", "filenames", "react-hooks"],
+  "rules": {
+    "no-unused-expressions": 0,
+    "array-func/prefer-flat-map": 2,
+    "array-func/prefer-flat": 1,
+    "arrow-parens": [2, "as-needed"],
+    "brace-style": [2, "stroustrup"],
+    "comma-dangle": [2, {
+      "arrays": "never",
+      "objects": "never",
+      "imports": "never",
+      "exports": "never",
+      "functions": "ignore"
+      }],
+    "filenames/match-regex": [2, "^([a-z0-9]+)([A-Z][a-z0-9]+)*\\.*(presenter|container|reducer|index|helper|async|shapes|selector)*.*$", true],
+    "fp/no-arguments": 2,
+    "fp/no-delete": 2,
+    "fp/no-get-set": 2,
+    "fp/no-let": 2,
+    "fp/no-loops": 2,
+    "fp/no-mutating-assign": 2,
+    "fp/no-mutating-methods": 1,
+    "fp/no-proxy": 2,
+    "fp/no-valueof-field": 2,
+    "import/prefer-default-export": 1,
+    "immutable/no-this": 1,
+    "jsx-quotes": [2, "prefer-single"],
+    "jsx-a11y/label-has-for": [2, {
+      "required": {
+        "some": ["nesting", "id"]
+      }
+    }],
+    "jsx-a11y/click-events-have-key-events": 0,
+    "no-multiple-empty-lines": [2, {
+      "max": 2,
+      "maxBOF": 0,
+      "maxEOF": 1
+    }],
+    "import/no-extraneous-dependencies": [2, {"devDependencies": ["**/*.stories.js", "**/*.test.js", "**/.storybook/*.js"]}],
+    "max-len": 0,
+    "no-console": [2, {
+      "allow": ["log", "warn", "error"]
+    }],
+    "no-var": 2,
+    "object-curly-spacing": [2, "never"],
+    "object-curly-newline": [2, {
+      "multiline": true,
+      "consistent": true
+    }],
+    "function-paren-newline": [2, "multiline"],
+    "operator-linebreak": [2, "before", {
+      "overrides": {"=": "after"}
+    }],
+    "implicit-arrow-linebreak": 0,
+    "react/jsx-filename-extension": 0,
+    "react/no-unused-prop-types": 1,
+    "react/require-default-props": 0,
+    "react/destructuring-assignment": 0,
+    "react-hooks/rules-of-hooks": 2,
+    "semi": [2, "never"],
+    "sonarjs/cognitive-complexity": [2, 15],
+    "space-in-parens": [2, "never"],
+    "strict": [2, "never"],
+    "unicorn/filename-case": [2, {"case": "camelCase"}],
+    "unicorn/no-abusive-eslint-disable": 2,
+    "unicorn/no-array-instanceof": 2,
+    "unicorn/prefer-starts-ends-with": 2,
+    "unicorn/regex-shorthand": 2,
+    "unicorn/prefer-spread": 2,
+    "unicorn/no-unsafe-regex": 2,
+    "unicorn/prefer-exponentiation-operator": 2
+  },
+  "globals": {
+    "console": false,
+    "document": false,
+    "fetch": false,
+    "Headers": false,
+    "it": false,
+    "navigator": false,
+    "URL": false,
+    "window": false,
+    "describe": false,
+    "expect": false
+  }
+}
